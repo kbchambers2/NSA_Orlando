c95184e90dcba3b367cdd43fa461817e487e1cd8
kbchambers2 <kcnsb24@aol.com>
2016-11-15 16:26:51 -0500

    Add files via upload

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,32 +1,47 @@
 import React, {Component} from 'react';
-import { Link } from 'react-router';
+import {Link} from 'react-router';
 import App from './App';
 import NavBar from './NavBar';
 import './styles/Home.css';
 
-
 class Home extends Component {
   render() {
     return (
-      <div>
-      <div className="navBarHome"><NavBar /></div>
-      <div className="topBox">
-        <p className="chapterName"><strong className="NSA">WELCOME </strong><i>NSA Orlando</i></p>
+      <div className="homeParentBox">
+        <div className="navBarHome"><NavBar/></div>
+        <div className="topBox">
+          <p className="chapterName">
+            <strong className="NSA">WELCOME </strong>
+            <i>NSA Orlando</i>
+          </p>
+        </div>
+        <div className="footerImages">
+          <ul className="imageList">
+            <li className="homeImages"><img src={require('../img/groupbowling.jpeg')} alt="Members"/>
+              <div className="gallery-text">
+                <h3>Check Out The Team</h3>
+              </div>
+            </li>
+            <li className="homeImages"><img src={require('../img/Downtown.jpg')} alt="Our Chapter"/>
+              <div className="gallery-text">
+                <h3>Our Chapter</h3>
+              </div>
+            </li>
+            <li className="homeImages"><img src={require('../img/calendar-photo.png')} alt="Calendar"/>
+              <div className="gallery-text">
+                <h3>What's Next</h3>
+              </div>
+            </li>
+            <li className="homeImages"><img src={require('../img/connect2.jpg')} alt="Connect"/>
+              <div className="gallery-text">
+                <h3>We Love Making
+                  <i>New Friends</i>
+                </h3>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className="footerImages">
-        <ul className="imageList">
-          <li>
-            <div className="membersBox">
-              <p>Members</p>
-              <p>Meet the people <br/> who make this chapter tick</p>
-            </div>
-          </li>
-          <li><img src={require('../img/Downtown.jpg')} alt="Our Chapter"/></li>
-          <li><img src={require('../img/calendar-photo.png')} alt="Calendar"/></li>
-          <li><img src="" alt="Connect"/></li>
-        </ul>
-      </div>
-    </div>
     );
   }
 }
